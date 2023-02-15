716884431e51865d35c4b9b0943ba057bc336008
Emma Paris <kevinroan@proton.me>
2023-02-16 01:14:50 +0530

    fixed image upload

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(formidableMiddleware());
 
 app.use("/", userRouter);
 app.use("/admin", adminRouter);
