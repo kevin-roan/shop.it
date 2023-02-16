67f59d1752fbb9e83e457e4480b51d5ceff6c1e0
Emma Paris <kevinroan@proton.me>
2023-02-16 12:56:44 +0530

    created login,signup pages

--- routes/user.js
+++ routes/user.js
@@ -9,4 +9,10 @@ router.get("/", function (req, res, next) {
     res.render("user/view-products", { admin: false, products });
   });
 });
+router.get("/login", (req, res) => {
+  res.render("user/login");
+});
+router.get("/signup", (req, res) => {
+  res.render("user/signup");
+});
 module.exports = router;
