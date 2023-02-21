f34608ecb3b2e2030887b036c6036eca0f3b90a1
kevin-roan <kevinroan@proton.me>
2023-02-21 09:01:38 +0530

    cart working af

--- helpers/user-helper.js
+++ helpers/user-helper.js
@@ -104,4 +104,17 @@ module.exports = {
       resolve(cartItems[0].cartItems);
     });
   },
+  getCartCount: (userId) => {
+    return new Promise(async (resolve, reject) => {
+      let count = 0;
+      let cart = await db
+        .get()
+        .collection(collection.CART_COLLECTION)
+        .findOne({ user: objectId(userId) });
+      if (cart) {
+        count = cart.products.length;
+      }
+      resolve(count);
+    });
+  },
 };
--- routes/user.js
+++ routes/user.js
@@ -12,11 +12,21 @@ const verifyLogin = (req, res, next) => {
   }
 };
 /* GET home page. */
-router.get("/", function (req, res, next) {
+router.get("/", async function (req, res, next) {
   let user = req.session.user;
   console.log(user);
+  let cartCount = null;
+  // when the user is not avail we cant access cart, so we need to check whether there is a user or not. by
+  if (req.session.user) {
+    cartCount = await userHelpers.getCartCount(req.session.user._id);
+  }
   productHelpers.getAllProducts().then((products) => {
-    res.render("user/view-products", { admin: false, products, user });
+    res.render("user/view-products", {
+      admin: false,
+      products,
+      user,
+      cartCount,
+    });
   });
 });
 router.get("/login", (req, res) => {
@@ -61,7 +71,7 @@ router.get("/logout", (req, res) => {
 router.get("/cart", verifyLogin, async (req, res) => {
   let products = await userHelpers.getCartProducts(req.session.user._id);
   console.log(products);
-  res.render("user/cart");
+  res.render("user/cart", { products, user: req.session.user });
 });
 
 router.get("/add-to-cart/:id", verifyLogin, (req, res) => {
