8551c41882587c4b49b91fd4e2b47abf0a9ef362
kevin-roan <kevinroan@proton.me>
2023-02-22 15:34:22 +0530

    fixed minor bug

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -14,3 +14,46 @@ function addToCart(proId) {
     },
   });
 }
+function changeQuantity(cartId, proId, count) {
+  // function changeQuantity(cartId,proId,count){
+  let quantity = parseInt(document.getElementById(proId).innerHTML);
+  console.log(quantity);
+  count = parseInt(count);
+
+  $.ajax({
+    url: "/change-product-quantity",
+    data: {
+      cart: cartId,
+      product: proId,
+      count: count,
+      quantity: quantity,
+    },
+    method: "post",
+    success: (response) => {
+      if (response.removeProduct) {
+        alert("Product Removed from cart");
+        location.reload();
+      } else {
+        document.getElementById(proId).innerHTML = quantity + count;
+      }
+    },
+  });
+}
+
+function removeProduct(cartId, proId) {
+  console.log("remove button pressed");
+  $.ajax({
+    url: "/remove-product",
+    data: {
+      cart: cartId,
+      product: proId,
+    },
+    method: "post",
+    success: (response) => {
+      if (response.removeProduct) {
+        alert("Product Removed from cart");
+        location.reload();
+      }
+    },
+  });
+}
