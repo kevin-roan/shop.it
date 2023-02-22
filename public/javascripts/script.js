857deaf8ac26b23010e0c2a9cf3f2173ec4bb401
kevin-roan <kevinroan@proton.me>
2023-02-22 12:31:44 +0530

    moved cart-change-quantity function to public/javascripts/

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -14,3 +14,19 @@ function addToCart(proId) {
     },
   });
 }
+
+<script>
+  function changeQuantity(cartId,proId,count)
+  {$.ajax({
+    url: "/change-product-quantity",
+    data: {
+      cart: cartId,
+      product: proId,
+      count: count,
+    },
+    method: "post",
+    success: (response) => {
+      alert(response);
+    },
+  })}
+</script>;
