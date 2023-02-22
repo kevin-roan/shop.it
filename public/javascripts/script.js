5cf32492a3a3cf0d27a8148b0e61d7e6fcdbe687
kevin-roan <kevinroan@proton.me>
2023-02-22 12:34:56 +0530

    fixed minor bug

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -14,19 +14,3 @@ function addToCart(proId) {
     },
   });
 }
-
-<script>
-  function changeQuantity(cartId,proId,count)
-  {$.ajax({
-    url: "/change-product-quantity",
-    data: {
-      cart: cartId,
-      product: proId,
-      count: count,
-    },
-    method: "post",
-    success: (response) => {
-      alert(response);
-    },
-  })}
-</script>;
